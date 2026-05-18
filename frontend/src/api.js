cef533cb6687e2a7445ddef3208d003130866f84
Teru-Terun <hack90x@gmail.com>
2026-05-18 10:01:51 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'http://127.0.0.1:8000';
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
+const BACKEND_URL = process.env.BACKEND_URL;
 
 const debug_api = axios.create({
   baseURL: API_BASE_URL,
